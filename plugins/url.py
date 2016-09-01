11960c231b5c82f7fe379798c124f5fde60e51d7
Rj48 <h_meirou@yahoo.co.jp>
2016-09-02 00:26:53 +0200

    added proper decoding for japanese page titles

--- plugins/url.py
+++ plugins/url.py
@@ -2,12 +2,16 @@
 import re
 import urllib2
 
-def getUrlTitle(url):
+def getUrlTitle(url, enc=['utf8', 'shift-jis', 'euc-jp']):
     regex = r'<title>(.+?)</title>'
     data = urllib2.urlopen(url).read(1024*8)
     title = re.findall(regex, data, re.IGNORECASE)
     if len(title) > 0:
-        return title[0]
+        for e in enc:
+            try:
+                return title[0].decode(e).encode('utf8')
+            except Exception as ex:
+                pass
 
 def url(bot, msg):
     regex = r'(https?://\S+)'
