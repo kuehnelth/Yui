828593d4cbd79296adb6c986cc7b4818e9a82ec3
Rj48 <h_meirou@yahoo.co.jp>
2016-09-02 15:12:17 +0200

    fixed crash on nonexistant urls

--- plugins/url.py
+++ plugins/url.py
@@ -4,20 +4,28 @@ import urllib2
 
 def getUrlTitle(url, enc=['utf8', 'shift-jis', 'euc-jp']):
     regex = r'<title>(.+?)</title>'
-    data = urllib2.urlopen(url).read(1024*8)
-    title = re.findall(regex, data, re.IGNORECASE)
-    if len(title) > 0:
-        for e in enc:
-            try:
-                return title[0].decode(e).encode('utf8')
-            except Exception as ex:
-                pass
+
+    try:
+        data = urllib2.urlopen(url).read(1024*8)
+        title = re.findall(regex, data, re.IGNORECASE)
+    except Exception as ex:
+        return None
+    else:
+        if len(title) > 0:
+            for e in enc:
+                try:
+                    return title[0].decode(e).encode('utf8')
+                except Exception as ex:
+                    pass
 
 def url(bot, msg):
     regex = r'(https?://\S+)'
     urls = re.findall(regex, msg.msg)
     for u in urls:
-        bot.sendMsg(msg.replyTo, 'Title: %s' % getUrlTitle(u))
+        title = getUrlTitle(u)
+        if not title:
+            title = 'no idea :('
+        bot.sendMsg(msg.replyTo, 'Title: %s' % title)
 
 
 def init(bot):
