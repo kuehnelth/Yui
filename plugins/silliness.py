5105fa8a54d63498eefe4d1d46231efabd0e4cf6
Rj48 <h_meirou@yahoo.co.jp>
2016-09-02 02:11:58 +0200

    silly answers for silly people

---
+++ plugins/silliness.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import re
+
+#responds to any message starting with the bot's nick
+#and otherwise only contains spaces or the caracters <>!?.o
+#which allows for some silly things like \o/ or >.< or !?!?!
+#if that's the case, the bot answers the same back to whomever mentioned him
+def sillyAnswers(bot,msg):
+    botnick = bot.nick.lower()
+    if msg.msg.lower().startswith(botnick):
+        suffix = msg.msg[len(botnick):]
+        if not suffix or re.match(r'^[o<>!\?\. ]+$',suffix) is not None:
+            bot.sendMsg(msg.replyTo, msg.user + suffix)
+
+def init(bot):
+    bot.events['channelMessage'].append(sillyAnswers)
