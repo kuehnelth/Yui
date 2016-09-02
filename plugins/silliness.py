39b4e29ba4dceb23dcfdda9135c45007a00d0e80
Rj48 <h_meirou@yahoo.co.jp>
2016-09-02 02:26:19 +0200

    forgot to add the slashes to the regex

--- plugins/silliness.py
+++ plugins/silliness.py
@@ -10,7 +10,7 @@ def sillyAnswers(bot,msg):
     botnick = bot.nick.lower()
     if msg.msg.lower().startswith(botnick):
         suffix = msg.msg[len(botnick):]
-        if not suffix or re.match(r'^[o<>!\?\. ]+$',suffix) is not None:
+        if not suffix or re.match(r'^[o<>/\\!\?\. ]+$',suffix) is not None:
             bot.sendMsg(msg.replyTo, msg.user + suffix)
 
 def init(bot):
