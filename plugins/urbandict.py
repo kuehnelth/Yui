730af3a9f19ed530938040aeefe8b96928f9dba1
Rj48 <h_meirou@yahoo.co.jp>
2016-09-03 03:30:23 +0200

    urban dictionary plugin

---
+++ plugins/urbandict.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import urllib2
+import json
+
+def ud(bot,msg):
+    if msg.msg.startswith('!ud'):
+        split = msg.msg.split(' ')
+        if len(split) < 2:
+            return
+
+        word = split[1]
+        definition = None
+
+        try:
+            url = 'http://api.urbandictionary.com/v0/define?term=%s' % word
+            resp = urllib2.urlopen(url)
+            js = json.loads(resp.read())
+            definition = js['list'][0]['definition']
+        except Exception as ex:
+            pass
+
+        if not definition:
+            answer = 'Couldn\t find "%s" :(' % word
+        else:
+            answer = '"%s": %s' % (word, definition)
+        bot.sendMsg(msg.replyTo, answer)
+
+def init(bot):
+    bot.events['channelMessage'].append(ud)
