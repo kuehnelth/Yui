4ee694b10efa2331d50032199c8fecec5ff78eaa
Rj48 <h_meirou@yahoo.co.jp>
2016-09-09 23:30:22 +0200

    improved <title> parsing

--- plugins/url.py
+++ plugins/url.py
@@ -4,31 +4,72 @@
 import re
 import urllib2
 
-def getUrlTitle(url, enc=['utf8', 'shift-jis', 'euc-jp']):
-    regex = r'<title>(.+?)</title>'
+from HTMLParser import HTMLParser
 
+class TitleParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.reading = False
+        self.done = False
+        self.title = ''
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'title':
+            self.reading = True
+
+    def handle_endtag(self, tag):
+        if tag == 'title':
+            self.reading = False
+            self.done = True
+
+    def handle_data(self, data):
+        if self.reading:
+            self.title += data
+
+    def handle_charref(self, ref):
+        if self.reading:
+            self.handle_entityref('#' + ref)
+
+    def handle_entityref(self, ref):
+        if self.reading:
+            self.title += '&%s;' % ref
+
+
+def getUrlTitle(url, enc=['utf8', 'shift-jis', 'ISO-8859', 'Windows-1251', 'euc-jp']):
+    title = ''
+    parser = TitleParser()
     try:
         resp = urllib2.urlopen(url)
-        content = ''
-        if 'content-type' in resp.headers:
-            enc.append(resp.headers['content-type'].split('charset=')[-1])
-        content = resp.read(1024*16)
-        title = re.findall(regex, content, re.IGNORECASE)
+
+        #try the charset set in the html header first, if there is one
+        #if 'content-type' in resp.headers:
+        #    enc.insert(0,enc.append(resp.headers['content-type'].split('charset=')[-1]))
+
+        for line in resp:
+            parser.feed(line)
+            if parser.done:
+                title = parser.title
+                break
+        parser.close()
     except Exception as ex:
         return None
     else:
         if len(title) > 0:
             for e in enc:
                 try:
-                    return title[0].decode(e)
+                    dec = title.decode(e)
+                    dec = parser.unescape(dec)
+                    return dec
                 except Exception as ex:
                     pass
+        return title
 
 def url(bot, msg):
     #don't react to stuff sent by the bot
     if msg.user == bot.nick:
         return
 
+    #find urls in channel message
     regex = r'(https?://\S+)'
     urls = re.findall(regex, msg.msg)
     for u in urls:
