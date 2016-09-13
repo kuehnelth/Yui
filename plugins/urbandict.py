8bafc755aa654a5a9ef5fc8d714eeb5bf8a8d377
Rj48 <h_meirou@yahoo.co.jp>
2016-09-13 21:38:26 +0200

    don't respond to messages by the bot itself

--- plugins/urbandict.py
+++ plugins/urbandict.py
@@ -6,33 +6,35 @@ import urllib2
 import json
 
 def ud(bot,msg):
-    if msg.msg.startswith('!ud'):
-        split = msg.msg.split(' ')
-        if len(split) < 2:
-            return
+    if not msg.msg.startswith('!ud') or msg.user == bot.nick:
+        return
 
-        word = split[1]
-        definition = None
-        idx = 0
+    split = msg.msg.split(' ')
+    if len(split) < 2:
+        return
 
-        try:
-            if len(split) > 2:
-                idx = int(split[2]) - 1
-            url = 'http://api.urbandictionary.com/v0/define?term=%s' % word
-            resp = urllib2.urlopen(url)
-            #get encoding
-            enc = resp.headers['content-type'].split('charset=')[-1]
-            content = unicode(resp.read(),enc)
-            js = json.loads(content)
-            definition = js['list'][idx]['definition']
-        except Exception as ex:
-            pass
+    word = split[1]
+    definition = None
+    idx = 0
 
-        if not definition:
-            answer = 'No results for "%s" :(' % word
-        else:
-            answer = '"%s": %s' % (word, definition)
-        bot.sendChannelMessage(msg.replyTo, answer)
+    try:
+        if len(split) > 2:
+            idx = int(split[2]) - 1
+        url = 'http://api.urbandictionary.com/v0/define?term=%s' % word
+        resp = urllib2.urlopen(url)
+        #get encoding
+        enc = resp.headers['content-type'].split('charset=')[-1]
+        content = unicode(resp.read(),enc)
+        js = json.loads(content)
+        definition = js['list'][idx]['definition']
+    except Exception as ex:
+        pass
+
+    if not definition:
+        answer = 'No results for "%s" :(' % word
+    else:
+        answer = '"%s": %s' % (word, definition)
+    bot.sendChannelMessage(msg.replyTo, answer)
 
 def init(bot):
     bot.events.register('channelMessage',ud)
