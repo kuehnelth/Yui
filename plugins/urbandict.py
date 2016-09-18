b81c171d79cc18edd88956586314f4d4ed7d9f74
Rj48 <h_meirou@yahoo.co.jp>
2016-09-18 13:26:05 +0200

    handle quotes in urbandict queries

--- plugins/urbandict.py
+++ plugins/urbandict.py
@@ -4,12 +4,27 @@
 
 import urllib2
 import json
+import csv
+
+#stupid hack taken from python.org csv examples, to get unicode to work
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
 
 def ud(bot,msg):
-    if not msg.msg.startswith('!ud') or msg.user == bot.nick:
+    if not msg.msg.startswith('!ud ') or msg.user == bot.nick:
         return
 
-    split = msg.msg.split(' ')
+    #split = msg.msg.split(' ')
+    split = unicode_csv_reader([msg.msg], delimiter=' ', quotechar='"', skipinitialspace=True).next()
     if len(split) < 2:
         return
 
@@ -20,7 +35,7 @@ def ud(bot,msg):
     try:
         if len(split) > 2:
             idx = int(split[2]) - 1
-        url = 'http://api.urbandictionary.com/v0/define?term=%s' % word
+        url = 'http://api.urbandictionary.com/v0/define?term=%s' % urllib2.quote(word.encode('utf-8'))
         resp = urllib2.urlopen(url)
         #get encoding
         enc = resp.headers['content-type'].split('charset=')[-1]
