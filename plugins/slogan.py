5189f779cbf6f6ac815e0ebe73751f167aefb102
Rj48 <h_meirou@yahoo.co.jp>
2016-10-02 23:19:57 +0200

    moar sloganz

--- plugins/slogan.py
+++ plugins/slogan.py
@@ -23,7 +23,30 @@ slogans = [
     u"Maybe she's born with it. Maybe it's {0}.",
     u"Don´t leave home without {0}",
     u"Think {0}.",
-    u"I'd walk a mile for a {0}."
+    u"I'd walk a mile for a {0}.",
+    u"Between {0} and madness lies obsession.",
+    u"Between love and madness lies {0}.",
+    u"Don't be {0}.",
+    u"Outwit. Outplay. {0}.",
+    u"Save {0}. Live Better.",
+    u"If you want to impress {0}, put him on your Black list.",
+    u"The {0} is always and completely {0}!",
+    u"The customer is always and completely {0}!",
+    u"When there is no {0}.",
+    u"At the heart of the {0}.",
+    u"The greatest tragedy is {0}.",
+    u"{0} is no substitute.",
+    u"Impossible is {0}.",
+    u"{0} is the path to joy.",
+    u"Pleasure is the path to {0}.",
+    u"Let your {0} do the walking.",
+    u"Because {0}'s complicated enough.",
+    u"Connecting {0}.",
+    u"{0}. It's a Mind Game.",
+    u"Power, beauty and {0}.",
+    u"{0}'s everywhere you want to be.",
+    u"Reach out and touch {0}.",
+    u"Get N or get {0}."
 ]
 
 def slogan(bot, msg):
