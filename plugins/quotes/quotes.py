1b2e227a393755a433312cbc1bd6b6b6d5078e29
Rj48 <h_meirou@yahoo.co.jp>
2017-01-02 07:49:10 +0100

    further converting of plugins

--- plugins/quotes/quotes.py
+++ plugins/quotes/quotes.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 import os
 import random
 import csv
@@ -8,54 +5,49 @@ import csv
 quoteDir = ''
 quoteList = {}
 
-def quote(bot, msg):
+#list tags and quotes in them
+@yui.command('qtags', 'qt')
+def qtags():
+    l = []
+    for t, i in quoteList.items():
+        l.append('%s(%d)' % (t, len(i)))
+    return 'Tags: ' + ', '.join(l)
+
+@yui.command('qadd','qa')
+@yui.perm('admin','moderator')
+def quote(argv,channel):
     global quoteList
     global quoteDir
 
-    isMod = msg.user in bot.config['moderators'] or msg.user in bot.config['admins']
-
-    split = list(csv.reader([msg.msg], delimiter=' ', quotechar='"', skipinitialspace=True))[0]
-
-    #list tags and quotes in them
-    if len(split) >= 1 and split[0] == '!qtags':
-        l = []
-        for t, i in quoteList.items():
-            l.append('%s(%d)' % (t, len(i)))
-        bot.sendMessage(msg.replyTo, 'Tags: ' + ', '.join(l))
-        return
-
     #store a quote
-    if isMod and len(split) > 1 and split[0] == '!qadd':
-        #figure out parameters
-        tag = msg.channel #default tag to channel name
-        content = split[1]
+    #figure out parameters
+    tag = channel #default tag to channel name
+    content = argv[1]
 
-        if len(split) > 2:
-            tag = split[1]
-            content = split[2]
-            #don't let people store quotes in some channel's specific tag
-            if tag.startswith('#'):
-                bot.sendMessage(msg.replyTo, 'No.')
-                return
+    if len(argv) > 2:
+        tag = argv[1]
+        content = argv[2]
+        #don't let people store quotes in some channel's specific tag
+        if tag.startswith('#'):
+            return 'No.'
 
-        storeQuote(tag, content)
-        bot.sendMessage(msg.replyTo, 'Stored quote in [%s]' % tag)
-        return
+    storeQuote(tag, content)
+    return 'Stored quote in [%s]' % tag
 
-    #recall quote
-    if len(split) >= 1 and split[0] == '!quote':
-        tag = msg.channel #default tag to channel name
+#recall quote
+@yui.command('quote','q')
+def quote(argv,channel):
+    tag = channel #default tag to channel name
 
-        if len(split) > 1:
-            tag = split[1]
-        if tag not in quoteList or len(quoteList[tag]) < 1:
-            bot.sendMessage(msg.replyTo, 'No tag named "%s" :(' % tag)
-        else:
-            l = len(quoteList[tag])
-            rnd = random.randint(0,l-1)
-            rcl = quoteList[tag][rnd]
-            bot.sendMessage(msg.replyTo, 'Quote for [%s] (%d/%d): %s' % (tag,rnd+1,l,rcl))
-        return
+    if len(argv) > 1:
+        tag = argv[1]
+    if tag not in quoteList or len(quoteList[tag]) < 1:
+        return 'No tag named "%s" :(' % tag
+    else:
+        l = len(quoteList[tag])
+        rnd = random.randint(0,l-1)
+        rcl = quoteList[tag][rnd]
+        return 'Quote for [%s] (%d/%d): %s' % (tag,rnd+1,l,rcl)
 
 #load existing quotes
 def loadQuotes():
@@ -92,14 +84,8 @@ def storeQuote(tag, msg):
             quoteList[tag] = []
         quoteList[tag].append(msg)
 
-def init(bot):
-    global quoteDir
-    quoteDir = os.path.dirname(__file__)
-    quoteDir = os.path.join(quoteDir, 'quotes')
-    if not os.path.exists(quoteDir):
-        os.makedirs(quoteDir)
-    loadQuotes()
-    bot.events.register('messageRecv',quote)
-
-def close(bot):
-    bot.events.unregister('messageRecv', quote)
+quoteDir = os.path.dirname(__file__)
+quoteDir = os.path.join(quoteDir, 'quotes')
+if not os.path.exists(quoteDir):
+    os.makedirs(quoteDir)
+loadQuotes()
