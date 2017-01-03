ebeff9cc83e74ee0e8091f9c98ca8f4453eb7c8e
Rj48 <h_meirou@yahoo.co.jp>
2017-01-03 21:41:01 +0100

    remove useless feature from greetings plug

--- plugins/greetings.py
+++ plugins/greetings.py
@@ -9,14 +9,12 @@ phrases = [
     ['よ', 'やぁ', 'おっす']
 ]
 
-
 def getRandomExcept(arr, ex):
     if not ex in arr:
         return random.choice(arr)
     idx = arr.index(ex)
     arrEx = arr[:idx] + arr[idx + 1:]
     return random.choice(arrEx)
-
 
 @yui.event('msgRecv')
 def greetings(msg, user, channel):
@@ -27,14 +25,4 @@ def greetings(msg, user, channel):
     lower = re.sub('[!?\. ]', '', lower)
     for s in phrases:
         if lower in s:
-            return getRandomExcept(s, lower)
-
-    # responds to any message starting with the bot's nick
-    # and otherwise only contains spaces or the caracters <>!?.o
-    # which allows for some things like \o/ or >.< or !?!?!
-    # if that's the case, the bot answers the same back to whomever mentioned him
-    botnick = yui.get_nick()
-    if msg.lower().startswith(botnick):
-        suffix = msg[len(botnick):]
-        if not suffix or re.match(r'^[o<>/\\!\?\. ]+$', suffix) is not None:
-            yui.send_msg(channel, user + suffix)
+            yui.send_msg(channel, getRandomExcept(s, lower))
